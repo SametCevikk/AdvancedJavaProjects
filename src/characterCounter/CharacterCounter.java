f52cfcd03b6cef82feaaba3d96beedd5b63913ff
Samet Cevik <ceviksamet302@gmail.com>
2025-04-07 15:31:18 +0300

    characterCounter package updated

--- src/characterCounter/CharacterCounter.java
+++ src/characterCounter/CharacterCounter.java
@@ -1,2 +1,29 @@
-package characterCounter;public class CharacterCounter {
+package characterCounter;
+
+import java.util.Scanner;
+
+public class CharacterCounter {
+
+    public static String countCharacter(String str){
+        StringBuffer sb = new StringBuffer();
+        int counter=1;
+        char currentChar=str.charAt(0);
+        for(int i=0;i<str.length();i++){
+            if(currentChar==str.charAt(i)){
+                counter++;
+            }else{
+                sb.append(counter).append(currentChar);
+                counter=1;
+                currentChar=str.charAt(i);
+            }
+        }
+        sb.append(counter).append(currentChar);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter string: ");
+        System.out.println(countCharacter(scanner.nextLine()));
+    }
 }
